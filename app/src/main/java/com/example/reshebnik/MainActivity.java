ef0c9062558abea6e96da50f9017bcb098314cd7
VictorDev <nirvana01_01@mail.ru>
2019-04-15 23:26:00 +0300

    частично добавил пошаговый вывод решения

--- app/src/main/java/com/example/reshebnik/MainActivity.java
+++ app/src/main/java/com/example/reshebnik/MainActivity.java
@@ -23,7 +23,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     boolean isFirstNumber = true;
     int lastIndexNumber;
-    LinearLayout linearLayout, linearLayout1, equationTvLayout;
+    LinearLayout linearLayout, linearLayout1, equationTvLayout, mainLayout;
     LayoutInflater layoutInflater;
     float fault;
     int limit1, limit2;
@@ -36,9 +36,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        linearLayout =findViewById(R.id.linearlayout);
+        linearLayout = findViewById(R.id.linearlayout);
         linearLayout1 = findViewById(R.id.equationLayout);
         equationTvLayout = findViewById(R.id.equationTvLayout);
+        mainLayout = findViewById(R.id.mainLayout);
         layoutInflater = getLayoutInflater();
     }
 
@@ -239,9 +240,11 @@ public class MainActivity extends AppCompatActivity {
 
     void foundRoot(){
         float f1,f2,f3;
+        float limit3;
         f1 = 0;
         f2 = 0;
-        while(){
+        //while(){
+            createTextLimit();
             int[] limits = {limit1,limit2};
             for(int j = 0; j<2; j++) {
                 float equationResult = 0;
@@ -274,13 +277,55 @@ public class MainActivity extends AppCompatActivity {
                 } else{
                     f2 = equationResult;
                 }
-            }
 
-        }
+               /* limit3 = limit1 - (f1*(limit2 - limit1)/(f2-f1));
+                Log.i("MyTag","limit3 is "+limit3); */
+            }
+        limit3 = limit1 - (f1*(limit2 - limit1)/(f2-f1));
+        Log.i("MyTag","limit3 is "+limit3);
+        createChord(f1,f2,limit3);
+
+        //}
         Log.i("MyTag","f1 is " + f1 + " f2 is " + f2);
     }
 
    /* boolean checkInterval(float a, float b, float c){
 
     } */
+
+   void createTextLimit(){
+       LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+       TextView tv = new TextView(this);
+       tv.setLayoutParams(layoutParams);
+       tv.setText("E = ["+limit1+";"+limit2+"]");
+       mainLayout.addView(tv);
+   }
+
+   void createChord(float f1, float f2, float limit3){
+
+       //отступить 4dp сверху для х= а и для = limit3
+       //добавить знак умножения для (в-а)
+       View chord = layoutInflater.inflate(R.layout.chord, mainLayout, false);
+
+       TextView tvA = chord.findViewById(R.id.a);
+       tvA.setText("x = "+ limit1 + " - ");
+
+       TextView tvFa = chord.findViewById(R.id.fa);
+       tvFa.setText(String.valueOf(f1));
+
+       TextView tvBa = chord.findViewById(R.id.ba);
+       String n = String.valueOf(limit2-limit1);
+       tvBa.setText(n);
+
+       TextView tvFb = chord.findViewById(R.id.fb);
+       tvFb.setText(String.valueOf(f2));
+
+       TextView tvFaB = chord.findViewById(R.id.faB);
+       tvFaB.setText(" - "+String.valueOf(f1));
+
+       TextView tvLimit3 = chord.findViewById(R.id.limit3tv);
+       tvLimit3.setText(" = " + String.valueOf(limit3));
+
+       mainLayout.addView(chord);
+   }
 }
