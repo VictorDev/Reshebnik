261b6d1c4d0ac3cc60b800a385c4f33a509ca752
VictorDev <nirvana01_01@mail.ru>
2019-04-15 15:08:35 +0300

    добавил расчет значений уравнения

--- app/src/main/java/com/example/reshebnik/MainActivity.java
+++ app/src/main/java/com/example/reshebnik/MainActivity.java
@@ -25,15 +25,12 @@ public class MainActivity extends AppCompatActivity {
     int lastIndexNumber;
     LinearLayout linearLayout, linearLayout1, equationTvLayout;
     LayoutInflater layoutInflater;
+    float fault;
+    int limit1, limit2;
     final int ID_REMOVE_BUTTON = 412351652;
     int[][]operands;
     float[][] operandsValue;
 
-    //выпадающее поле для коэффициента
-    //убрать лишние float значения в тексте
-    //убрать первый плюс
-    //кривое отображение = 0
-    //добавить выравнивание по левой стороге для экспонента
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,15 +83,14 @@ public class MainActivity extends AppCompatActivity {
             EditText sign = myView.findViewById(R.id.sign);
             if(sign.getText().toString().trim().length()>0){
                 if(sign.getText().toString().equals("+")){
-                    operands[i][0] = 0;
+                    operands[i][0] = 1;
                 Log.i("MyTag","sign is +");
 
                 } else{
-                    operands[i][0] = 1;
+                    operands[i][0] = -1;
                     Log.i("MyTag","sign is -");
                 }
             } else{
-                //оп
                 sign.setBackgroundColor(Color.RED);
                 return;
             }
@@ -143,43 +139,78 @@ public class MainActivity extends AppCompatActivity {
                 operands[i][3] = 0;
                 Log.i("MyTag", "not factor");
             }
-        }
+
+            EditText faultET = findViewById(R.id.fault);
+            if(faultET.getText().toString().trim().length()>0){
+                String e = faultET.getText().toString();
+                fault = Float.parseFloat(e);
+            }else{
+                faultET.setBackgroundColor(Color.RED);
+                return;
+            }
+
+            EditText limit1ET = findViewById(R.id.etX1);
+            if(limit1ET.getText().toString().trim().length()>0){
+                String e = limit1ET.getText().toString();
+                limit1 = Integer.parseInt(e);
+            }else{
+                limit1ET.setBackgroundColor(Color.RED);
+                return;
+            }
+
+            EditText limit2ET = findViewById(R.id.etX2);
+            if(limit2ET.getText().toString().trim().length()>0){
+                String e = limit2ET.getText().toString();
+                limit2 = Integer.parseInt(e);
+            }else{
+                limit2ET.setBackgroundColor(Color.RED);
+                return;
+            }
+
+
+
+            }
         createTextViewEquation();
     }
 
     private void createTextViewEquation(){
-        for(int i =0; i<=lastIndexNumber; i++){
+        for(int i =0; i<=lastIndexNumber; i++) {
             View item = layoutInflater.inflate(R.layout.operandtv, equationTvLayout, false);
             TextView sign = item.findViewById(R.id.signtv);
-            if(operands[i][0]==0){
+            if (operands[i][0] == 1 && i == 0) {
+                sign.setText("");
+            } else if (operands[i][0] == 1) {
                 sign.setText("+");
-            }else{
+            } else {
                 sign.setText("-");
             }
 
             TextView number = item.findViewById(R.id.numbertv);
-            if(operands[i][1]==0){
+            if (operands[i][1] == 0) {
                 number.setText("X");
-            }else{
+            } else {
                 String n = String.valueOf(operandsValue[i][1]);
                 String[] splitter = n.split("\\.");
                 int c = splitter[1].length();
-                if(c>1){
+                if (c > 1) {
                     number.setText(n);
-                }else{
-                    String n1 = String.valueOf((int)operandsValue[i][1]);
+                } else {
+                    String n1 = String.valueOf((int) operandsValue[i][1]);
                     number.setText(n1);
                 }
 
             }
 
             TextView exponent = item.findViewById(R.id.exponenttv);
-            if(operands[i][2]==0){
+            if (operands[i][2] == 0) {
                 exponent.setText("x");
-            }else{
-                String n = String.valueOf((int)operandsValue[i][2]);
+            } else if (operandsValue[i][2] == 1.0) {
+                exponent.setText("");
+            } else {
+                String n = String.valueOf((int) operandsValue[i][2]);
                 exponent.setText(n);
             }
+
 
             TextView factor = item.findViewById(R.id.factortv);
             if(operands[i][3]==1){
@@ -199,11 +230,57 @@ public class MainActivity extends AppCompatActivity {
             equationTvLayout.addView(item);
 
         }
-        LayoutParams lpView = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 
-        TextView tv = new TextView(this);
-        tv.setText(" = 0");
-        tv.setLayoutParams(lpView);
-        equationTvLayout.addView(tv);
+        View zero = layoutInflater.inflate(R.layout.zero, equationTvLayout, false);
+        equationTvLayout.addView(zero);
+
+        foundRoot();
     }
+
+    void foundRoot(){
+        float f1,f2,f3;
+        f1 = 0;
+        f2 = 0;
+        while(){
+            int[] limits = {limit1,limit2};
+            for(int j = 0; j<2; j++) {
+                float equationResult = 0;
+                for (int i = 0; i <= lastIndexNumber; i++) {
+                    int sign = operands[i][0];
+                    float factor;
+                    if (operands[i][3] == 1) {
+                        factor = operandsValue[i][3];
+                    } else {
+                        factor = 1;
+                    }
+                    float number;
+                    if (operands[i][1] == 0) {
+                        number = limits[j];
+                    } else {
+                        number = operandsValue[i][1];
+                    }
+                    float exponent;
+                    if (operands[i][2]==0){
+                        exponent = limits[j];
+                    }else{
+                        exponent = operandsValue[i][2];
+                    }
+                    double valueOperand = sign*factor*Math.pow(number,exponent);
+                    float spareValue = (float) valueOperand;
+                    equationResult+=spareValue;
+                }
+                if(j==0){
+                    f1 = equationResult;
+                } else{
+                    f2 = equationResult;
+                }
+            }
+
+        }
+        Log.i("MyTag","f1 is " + f1 + " f2 is " + f2);
+    }
+
+   /* boolean checkInterval(float a, float b, float c){
+
+    } */
 }
