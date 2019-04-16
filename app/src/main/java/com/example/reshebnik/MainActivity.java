a21dd993f5dcde7447aa77d1efd3b0caab24fc1e
VictorDev <nirvana01_01@mail.ru>
2019-04-16 12:24:11 +0300

    добавил подробный пошаговый вывод решения

--- app/src/main/java/com/example/reshebnik/MainActivity.java
+++ app/src/main/java/com/example/reshebnik/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     final int ID_REMOVE_BUTTON = 412351652;
     int[][]operands;
     float[][] operandsValue;
+    LayoutParams layoutParams;
 
 
     @Override
@@ -40,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         linearLayout1 = findViewById(R.id.equationLayout);
         equationTvLayout = findViewById(R.id.equationTvLayout);
         mainLayout = findViewById(R.id.mainLayout);
+        layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
         layoutInflater = getLayoutInflater();
     }
 
@@ -283,10 +285,17 @@ public class MainActivity extends AppCompatActivity {
             }
         limit3 = limit1 - (f1*(limit2 - limit1)/(f2-f1));
         Log.i("MyTag","limit3 is "+limit3);
+        createFValue(f1,f2);
         createChord(f1,f2,limit3);
+        createNewInterval(limit3);
+
 
         //}
         Log.i("MyTag","f1 is " + f1 + " f2 is " + f2);
+    }
+
+    void calculateEquation(float limit){
+        
     }
 
    /* boolean checkInterval(float a, float b, float c){
@@ -294,7 +303,6 @@ public class MainActivity extends AppCompatActivity {
     } */
 
    void createTextLimit(){
-       LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
        TextView tv = new TextView(this);
        tv.setLayoutParams(layoutParams);
        tv.setText("E = ["+limit1+";"+limit2+"]");
@@ -308,14 +316,14 @@ public class MainActivity extends AppCompatActivity {
        View chord = layoutInflater.inflate(R.layout.chord, mainLayout, false);
 
        TextView tvA = chord.findViewById(R.id.a);
-       tvA.setText("x = "+ limit1 + " - ");
+       tvA.setText("x = "+ limit1 + " -   ");
 
        TextView tvFa = chord.findViewById(R.id.fa);
        tvFa.setText(String.valueOf(f1));
 
        TextView tvBa = chord.findViewById(R.id.ba);
        String n = String.valueOf(limit2-limit1);
-       tvBa.setText(n);
+       tvBa.setText(" * "+n);
 
        TextView tvFb = chord.findViewById(R.id.fb);
        tvFb.setText(String.valueOf(f2));
@@ -324,8 +332,29 @@ public class MainActivity extends AppCompatActivity {
        tvFaB.setText(" - "+String.valueOf(f1));
 
        TextView tvLimit3 = chord.findViewById(R.id.limit3tv);
-       tvLimit3.setText(" = " + String.valueOf(limit3));
+       tvLimit3.setText("   = " + String.valueOf(limit3));
 
        mainLayout.addView(chord);
+   }
+
+   void createFValue(float f1, float f2){
+       TextView tvValue = new TextView(this);
+       tvValue.setLayoutParams(layoutParams);
+       tvValue.setText("f("+limit1+") = "+f1 +"\n f("+limit2+") = "+f2);
+       mainLayout.addView(tvValue);
+   }
+
+   void createValue(float f3, float limit3){
+       TextView tvValue = new TextView(this);
+       tvValue.setLayoutParams(layoutParams);
+       tvValue.setText("f("+limit3+") = "+f3);
+       mainLayout.addView(tvValue);
+   }
+
+   void createNewInterval(float limit3){
+       TextView tvValue = new TextView(this);
+       tvValue.setLayoutParams(layoutParams);
+       tvValue.setText("x е ["+limit1+";"+limit3 + "] и ["+limit3+";"+limit2+"]");
+       mainLayout.addView(tvValue);
    }
 }
